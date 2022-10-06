3f5a6dacb6de34413f78570467107c95e5063483
BandySim <dalei10311997@gmail.com>
2022-10-06 19:53:11 +0800

    First Com

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <NavbarComponents class="index"/>
+    <DashboardComponents/>
+    <FooterComponents />
+  </v-app>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavbarComponents from '@/components/Navbar-components.vue';
+import DashboardComponents from '@/components/Dashboard-components.vue';
+import FooterComponents from '@/components/Footer-components.vue';
+
+export default {
+  name: 'HomeView',
+
+  components: {
+    NavbarComponents,
+    DashboardComponents,
+    FooterComponents
+}
+}
+</script>
